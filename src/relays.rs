83feac0c66da3e2dc9ccb6dd2568d227a71d9ff3
triesap <triesap@radroots.dev>
2026-01-08 16:08:28 +0000

    groups: use core group helpers
    - route group tool handlers to core module
    - update args imports to core exports
    - keep event kinds and tags unchanged
    - remove duplicate group builder logic

--- src/relays.rs
+++ src/relays.rs
@@ -7,9 +7,8 @@ use nostr_mcp_core::relays as core_relays;
 use nostr_mcp_core::events as core_events;
 use nostr_mcp_core::replies as core_replies;
 use nostr_mcp_core::polls as core_polls;
+use nostr_mcp_core::groups as core_groups;
 use nostr_sdk::prelude::*;
-use schemars::JsonSchema;
-use serde::Deserialize;
 use std::collections::HashMap;
 
 pub use nostr_mcp_core::publish::{PostGroupChatArgs, PostReactionArgs, PostTextArgs, PostThreadArgs, SendResult};
@@ -17,95 +16,10 @@ pub use nostr_mcp_core::replies::{PostCommentArgs, PostReplyArgs};
 pub use nostr_mcp_core::polls::{
     CreatePollArgs, GetPollResultsArgs, PollOption, PollResultOption, PollResults, VotePollArgs,
 };
-
-#[derive(Debug, Deserialize, JsonSchema)]
-pub struct PutUserArgs {
-    pub content: String,
-    pub group_id: String,
-    pub pubkey: String,
-    pub roles: Option<Vec<String>>,
-    pub previous_refs: Option<Vec<String>>,
-    pub pow: Option<u8>,
-    pub to_relays: Option<Vec<String>>,
-}
-
-#[derive(Debug, Deserialize, JsonSchema)]
-pub struct RemoveUserArgs {
-    pub content: String,
-    pub group_id: String,
-    pub pubkey: String,
-    pub previous_refs: Option<Vec<String>>,
-    pub pow: Option<u8>,
-    pub to_relays: Option<Vec<String>>,
-}
-
-#[derive(Debug, Deserialize, JsonSchema)]
-pub struct EditGroupMetadataArgs {
-    pub content: String,
-    pub group_id: String,
-    pub name: Option<String>,
-    pub picture: Option<String>,
-    pub about: Option<String>,
-    pub public: Option<bool>,
-    pub open: Option<bool>,
-    pub previous_refs: Option<Vec<String>>,
-    pub pow: Option<u8>,
-    pub to_relays: Option<Vec<String>>,
-}
-
-#[derive(Debug, Deserialize, JsonSchema)]
-pub struct DeleteEventArgs {
-    pub content: String,
-    pub group_id: String,
-    pub event_id: String,
-    pub previous_refs: Option<Vec<String>>,
-    pub pow: Option<u8>,
-    pub to_relays: Option<Vec<String>>,
-}
-
-#[derive(Debug, Deserialize, JsonSchema)]
-pub struct CreateGroupArgs {
-    pub content: String,
-    pub group_id: String,
-    pub previous_refs: Option<Vec<String>>,
-    pub pow: Option<u8>,
-    pub to_relays: Option<Vec<String>>,
-}
-
-#[derive(Debug, Deserialize, JsonSchema)]
-pub struct DeleteGroupArgs {
-    pub content: String,
-    pub group_id: String,
-    pub previous_refs: Option<Vec<String>>,
-    pub pow: Option<u8>,
-    pub to_relays: Option<Vec<String>>,
-}
-
-#[derive(Debug, Deserialize, JsonSchema)]
-pub struct CreateInviteArgs {
-    pub content: String,
-    pub group_id: String,
-    pub previous_refs: Option<Vec<String>>,
-    pub pow: Option<u8>,
-    pub to_relays: Option<Vec<String>>,
-}
-
-#[derive(Debug, Deserialize, JsonSchema)]
-pub struct JoinGroupArgs {
-    pub content: String,
-    pub group_id: String,
-    pub invite_code: Option<String>,
-    pub pow: Option<u8>,
-    pub to_relays: Option<Vec<String>>,
-}
-
-#[derive(Debug, Deserialize, JsonSchema)]
-pub struct LeaveGroupArgs {
-    pub content: String,
-    pub group_id: String,
-    pub pow: Option<u8>,
-    pub to_relays: Option<Vec<String>>,
-}
+pub use nostr_mcp_core::groups::{
+    CreateGroupArgs, CreateInviteArgs, DeleteEventArgs, DeleteGroupArgs, EditGroupMetadataArgs,
+    JoinGroupArgs, LeaveGroupArgs, PutUserArgs, RemoveUserArgs,
+};
 
 pub async fn set_relays(client: &Client, args: RelaysSetArgs) -> Result<()> {
     core_relays::set_relays(client, args)
@@ -239,226 +153,55 @@ pub async fn get_poll_results(
 }
 
 pub async fn put_user(client: &Client, args: PutUserArgs) -> Result<SendResult> {
-    use crate::error::GoostrError;
-    
-    let pubkey = PublicKey::from_hex(&args.pubkey)
-        .map_err(|e| GoostrError::InvalidPublicKey(format!("{}: {}", args.pubkey, e)))?;
-
-    let mut tags = Vec::new();
-
-    tags.push(Tag::parse(&["h".to_string(), args.group_id.clone()])?);
-
-    let mut p_tag = vec!["p".to_string(), pubkey.to_hex()];
-    if let Some(roles) = args.roles {
-        for role in roles {
-            p_tag.push(role);
-        }
-    }
-    tags.push(Tag::parse(&p_tag)?);
-
-    if let Some(refs) = args.previous_refs {
-        for ref_id in refs {
-            tags.push(Tag::parse(&["previous".to_string(), ref_id])?);
-        }
-    }
-
-    let mut builder = EventBuilder::new(Kind::from(9000), args.content).tags(tags);
-
-    if let Some(pow) = args.pow {
-        builder = builder.pow(pow);
-    }
-
-    publish_event_builder(client, builder, args.to_relays).await
+    core_groups::put_user(client, args)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))
 }
 
 pub async fn remove_user(client: &Client, args: RemoveUserArgs) -> Result<SendResult> {
-    use crate::error::GoostrError;
-    
-    let pubkey = PublicKey::from_hex(&args.pubkey)
-        .map_err(|e| GoostrError::InvalidPublicKey(format!("{}: {}", args.pubkey, e)))?;
-
-    let mut tags = Vec::new();
-
-    tags.push(Tag::parse(&["h".to_string(), args.group_id.clone()])?);
-    tags.push(Tag::parse(&["p".to_string(), pubkey.to_hex()])?);
-
-    if let Some(refs) = args.previous_refs {
-        for ref_id in refs {
-            tags.push(Tag::parse(&["previous".to_string(), ref_id])?);
-        }
-    }
-
-    let mut builder = EventBuilder::new(Kind::from(9001), args.content).tags(tags);
-
-    if let Some(pow) = args.pow {
-        builder = builder.pow(pow);
-    }
-
-    publish_event_builder(client, builder, args.to_relays).await
+    core_groups::remove_user(client, args)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))
 }
 
 pub async fn edit_group_metadata(client: &Client, args: EditGroupMetadataArgs) -> Result<SendResult> {
-    let mut tags = Vec::new();
-
-    tags.push(Tag::parse(&["h".to_string(), args.group_id.clone()])?);
-
-    if let Some(name) = args.name {
-        tags.push(Tag::parse(&["name".to_string(), name])?);
-    }
-
-    if let Some(picture) = args.picture {
-        tags.push(Tag::parse(&["picture".to_string(), picture])?);
-    }
-
-    if let Some(about) = args.about {
-        tags.push(Tag::parse(&["about".to_string(), about])?);
-    }
-
-    if let Some(public) = args.public {
-        if public {
-            tags.push(Tag::parse(&["public".to_string()])?);
-        } else {
-            tags.push(Tag::parse(&["private".to_string()])?);
-        }
-    }
-
-    if let Some(open) = args.open {
-        if open {
-            tags.push(Tag::parse(&["open".to_string()])?);
-        } else {
-            tags.push(Tag::parse(&["closed".to_string()])?);
-        }
-    }
-
-    if let Some(refs) = args.previous_refs {
-        for ref_id in refs {
-            tags.push(Tag::parse(&["previous".to_string(), ref_id])?);
-        }
-    }
-
-    let mut builder = EventBuilder::new(Kind::from(9002), args.content).tags(tags);
-
-    if let Some(pow) = args.pow {
-        builder = builder.pow(pow);
-    }
-
-    publish_event_builder(client, builder, args.to_relays).await
+    core_groups::edit_group_metadata(client, args)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))
 }
 
 pub async fn delete_group_event(client: &Client, args: DeleteEventArgs) -> Result<SendResult> {
-    use crate::error::GoostrError;
-    
-    let event_id = EventId::from_hex(&args.event_id)
-        .map_err(|e| GoostrError::InvalidEventId(format!("{}: {}", args.event_id, e)))?;
-
-    let mut tags = Vec::new();
-
-    tags.push(Tag::parse(&["h".to_string(), args.group_id.clone()])?);
-    tags.push(Tag::parse(&["e".to_string(), event_id.to_hex()])?);
-
-    if let Some(refs) = args.previous_refs {
-        for ref_id in refs {
-            tags.push(Tag::parse(&["previous".to_string(), ref_id])?);
-        }
-    }
-
-    let mut builder = EventBuilder::new(Kind::from(9005), args.content).tags(tags);
-
-    if let Some(pow) = args.pow {
-        builder = builder.pow(pow);
-    }
-
-    publish_event_builder(client, builder, args.to_relays).await
+    core_groups::delete_group_event(client, args)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))
 }
 
 pub async fn create_group(client: &Client, args: CreateGroupArgs) -> Result<SendResult> {
-    let mut tags = Vec::new();
-
-    tags.push(Tag::parse(&["h".to_string(), args.group_id.clone()])?);
-
-    if let Some(refs) = args.previous_refs {
-        for ref_id in refs {
-            tags.push(Tag::parse(&["previous".to_string(), ref_id])?);
-        }
-    }
-
-    let mut builder = EventBuilder::new(Kind::from(9007), args.content).tags(tags);
-
-    if let Some(pow) = args.pow {
-        builder = builder.pow(pow);
-    }
-
-    publish_event_builder(client, builder, args.to_relays).await
+    core_groups::create_group(client, args)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))
 }
 
 pub async fn delete_group(client: &Client, args: DeleteGroupArgs) -> Result<SendResult> {
-    let mut tags = Vec::new();
-
-    tags.push(Tag::parse(&["h".to_string(), args.group_id.clone()])?);
-
-    if let Some(refs) = args.previous_refs {
-        for ref_id in refs {
-            tags.push(Tag::parse(&["previous".to_string(), ref_id])?);
-        }
-    }
-
-    let mut builder = EventBuilder::new(Kind::from(9008), args.content).tags(tags);
-
-    if let Some(pow) = args.pow {
-        builder = builder.pow(pow);
-    }
-
-    publish_event_builder(client, builder, args.to_relays).await
+    core_groups::delete_group(client, args)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))
 }
 
 pub async fn create_invite(client: &Client, args: CreateInviteArgs) -> Result<SendResult> {
-    let mut tags = Vec::new();
-
-    tags.push(Tag::parse(&["h".to_string(), args.group_id.clone()])?);
-
-    if let Some(refs) = args.previous_refs {
-        for ref_id in refs {
-            tags.push(Tag::parse(&["previous".to_string(), ref_id])?);
-        }
-    }
-
-    let mut builder = EventBuilder::new(Kind::from(9009), args.content).tags(tags);
-
-    if let Some(pow) = args.pow {
-        builder = builder.pow(pow);
-    }
-
-    publish_event_builder(client, builder, args.to_relays).await
+    core_groups::create_invite(client, args)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))
 }
 
 pub async fn join_group(client: &Client, args: JoinGroupArgs) -> Result<SendResult> {
-    let mut tags = Vec::new();
-
-    tags.push(Tag::parse(&["h".to_string(), args.group_id.clone()])?);
-
-    if let Some(code) = args.invite_code {
-        tags.push(Tag::parse(&["code".to_string(), code])?);
-    }
-
-    let mut builder = EventBuilder::new(Kind::from(9021), args.content).tags(tags);
-
-    if let Some(pow) = args.pow {
-        builder = builder.pow(pow);
-    }
-
-    publish_event_builder(client, builder, args.to_relays).await
+    core_groups::join_group(client, args)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))
 }
 
 pub async fn leave_group(client: &Client, args: LeaveGroupArgs) -> Result<SendResult> {
-    let mut tags = Vec::new();
-
-    tags.push(Tag::parse(&["h".to_string(), args.group_id.clone()])?);
-
-    let mut builder = EventBuilder::new(Kind::from(9022), args.content).tags(tags);
-
-    if let Some(pow) = args.pow {
-        builder = builder.pow(pow);
-    }
-
-    publish_event_builder(client, builder, args.to_relays).await
+    core_groups::leave_group(client, args)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))
 }
